8003a7ebaabb45c7557c397f21ffb212b9b35e9c
Vento <Michael.vento@student.hepl.be>
2019-12-05 11:19:40 +0100

    Put project in an object and local function

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,20 @@
-document.documentElement.classList.add('js-enabled');
-const nav = document.querySelector('#main');
-let topNav = nav.offsetTop;
-
-function fixNav() {
-    if (window.scrollY > topNav) {
-        document.body.classList.add('fixed-nav');
-    } else {
-        document.body.classList.remove('fixed-nav');
-    }
-}
-window.addEventListener('scroll', fixNav);
+(function stickyNav (){
+    const NavStick = {
+        init() {
+            document.documentElement.classList.add('js-enabled');
+            this.nav = document.querySelector('#main');
+            this.topNav = this.nav.offsetTop;
+            this.addEventListeners();
+        },
+        addEventListeners() {
+            window.addEventListener('scroll', () => {
+                if (window.scrollY > this.topNav) {
+                    document.body.classList.add('fixed-nav');
+                } else {
+                    document.body.classList.remove('fixed-nav');
+                }
+            });
+        }
+};
+NavStick.init();
+}());
